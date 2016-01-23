5f99e5c8bfcfeb0b54e82c63d430ab091e582ad6
Scott Sanderson <ssanderson@quantopian.com>
2016-01-23 00:08:56 -0500

    DOC: Add a README.

---
+++ straitlets/__init__.py
@@ -0,0 +1,27 @@
+from .serializable import Serializable
+from .traits import (
+    Bool,
+    Dict,
+    Enum,
+    Float,
+    Instance,
+    Integer,
+    List,
+    Set,
+    Tuple,
+    Unicode,
+)
+
+__all__ = (
+    'Bool',
+    'Dict',
+    'Enum',
+    'Float',
+    'Instance',
+    'Integer',
+    'List',
+    'Set',
+    'Tuple',
+    'Unicode',
+    'Serializable',
+)
