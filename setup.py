ce65050f189232e7ae76c3ecf8f31b8b6ec5011d
Scott Sanderson <ssanderson@quantopian.com>
2016-01-26 11:24:08 -0500

    MAINT: Bump PyPI version.

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ def extras_require():
 def main():
     setup(
         name='straitlets',
-        version='0.2.0',
+        version='0.2.1',
         description="Serializable IPython Traitlets",
         author="Quantopian Team",
         author_email="opensource@quantopian.com",
