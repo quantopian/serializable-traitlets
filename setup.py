111270aacc1c47baf634181bde4d33c73cf85bb2
Scott Sanderson <ssanderson@quantopian.com>
2016-02-01 14:02:14 -0500

    BUILD: PyPI commit.

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ def extras_require():
 def main():
     setup(
         name='straitlets',
-        version='0.2.3',
+        version='0.2.4',
         description="Serializable IPython Traitlets",
         author="Quantopian Team",
         author_email="opensource@quantopian.com",
